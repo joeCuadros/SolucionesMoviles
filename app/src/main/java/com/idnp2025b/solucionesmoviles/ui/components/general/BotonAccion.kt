4637fb8b7e42c89f68d7a9a1b73db65b76e93a8d
joeCuadros <jcuadrosam@unsa.edu.pe>
2025-11-22 02:50:56 -0500

    feat: Correccion en el componente de botonAccion dentro de general

--- app/src/main/java/com/idnp2025b/solucionesmoviles/ui/components/general/BotonAccion.kt
+++ app/src/main/java/com/idnp2025b/solucionesmoviles/ui/components/general/BotonAccion.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.idnp2025b.solucionesmoviles.ui.theme.colorVerdeEstado
 import com.idnp2025b.solucionesmoviles.ui.theme.colorAmarilloEstado
@@ -37,7 +38,12 @@ fun RowScope.BotonAccion(
             contentColor = colorTextoCalculado
         )
     ) {
-        Text(texto)
+        Text(
+            text = texto,
+            style = MaterialTheme.typography.labelMedium,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
     }
 }
 
